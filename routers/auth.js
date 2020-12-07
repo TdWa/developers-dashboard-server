d500ac0cf8ad1ab42e35554819140a433144135e
alexiordachescu <alex.iordachescu27@gmail.com>
2020-12-07 14:04:25 +0100

    add jwt and middleware

---
+++ routers/auth.js
@@ -0,0 +1,31 @@
+const bcrypt = require("bcrypt");
+const { Router } = require("express");
+const { toJWT } = require("../auth/jwt");
+const { SALT_ROUNDS } = require("../config/constants");
+const User = require("../models/").user;
+
+const router = new Router();
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .send({ message: "Please provide both an email and a password" });
+    }
+    const user = await User.findOne({ where: { email } });
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      return res.status(400).send({
+        message: "User with that email not found or password incorrect",
+      });
+    }
+    delete user.dataValues["password"];
+    const token = toJWT({ userId: user.id });
+    return res.status(200).send({ token, ...user.dataValues });
+  } catch (e) {
+    console.log(e);
+    return res.status(400).send({ message: "Something went wrong, sorry" });
+  }
+});
