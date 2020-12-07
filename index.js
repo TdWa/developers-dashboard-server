fcd9f722119cbd6a469ded1316f6611b6fc91b12
TdWa <59452572+TdWa@users.noreply.github.com>
2020-12-07 12:50:39 +0100

    add query test route

--- index.js
+++ index.js
@@ -2,14 +2,27 @@ const express = require("express");
 const app = express();
 const { PORT } = require("./config/constants");
 const cors = require("cors");
+app.set("json spaces", 2);
 
 // MiddleWares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/", (req, res, next) => {
-  res.json("Hello World!");
+const User = require("./models").user;
+
+// app.get("/", (req, res, next) => {
+//   res.json("Hello World!");
+// });
+
+app.get("/", async (req, res, next) => {
+  try {
+    const users = await User.findAll();
+
+    res.status(200).send(users);
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
