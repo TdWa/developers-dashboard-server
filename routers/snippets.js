fa98b872be4651ce24f570099df3406a8cd86bac
oliverclaxton <oliver.claxton66@gmail.com>
2020-12-08 11:49:37 +0100

    edit snippet endpoint

--- routers/snippets.js
+++ routers/snippets.js
@@ -4,12 +4,32 @@ const Snippet = require("../models/").snippet;
 const router = new Router();
 const auth = require("../auth/middleware");
 
+router.patch("/", auth, async (req, res, next) => {
+  try {
+    console.log("i got hetre");
+    const { content, id } = req.body;
+
+    const idToFind = parseInt(id);
+
+    console.log("what is req.body?", req.body);
+
+    const snippet = await Snippet.findByPk(idToFind);
+
+    await snippet.update({ content });
+
+    return res.status(201).send({ message: " snippet updated", snippet });
+  } catch (e) {
+    console.log("i am error", e.message);
+    next();
+  }
+});
+
 router.get("/", auth, async (req, res, next) => {
   try {
-    console.log("i am req.user", req.user);
+    // console.log("i am req.user", req.user);
     const id = req.user.dataValues.id;
     parseInt(id);
-    console.log("userId is  ", id);
+    // console.log("userId is  ", id);
 
     const allSnippetsByUserId = await Snippet.findAll({
       where: { userId: id },
