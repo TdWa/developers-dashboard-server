2a6b8871e5a887a0c0c6b8fd463a81fd9519e480
alexiordachescu <alex.iordachescu27@gmail.com>
2020-12-08 15:06:39 +0100

    create /deleteLink endpoint

--- routers/links.js
+++ routers/links.js
@@ -22,4 +22,38 @@ router.get("/", auth, async (req, res, next) => {
   }
 });
 
+router.delete("/deleteLink", async (req, res) => {
+  try {
+    const { id } = req.body;
+    const toDelete = await Link.findByPk(id);
+    if (!toDelete) {
+      res.status(404).send("Link not found");
+    } else {
+      await toDelete.destroy();
+      res.json(toDelete);
+    }
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+// create a link
+router.post("/", auth, async (req, res, next) => {
+  try {
+    const userId = req.user.dataValues.id;
+    const { categoryId, name, content } = req.body;
+    const newLink = await Link.create({
+      userId,
+      categoryId,
+      name,
+      content,
+    });
+
+    res.status(200).send(newLink);
+  } catch (e) {
+    console.log("i am error message", e.message);
+    next();
+  }
+});
+
 module.exports = router;
